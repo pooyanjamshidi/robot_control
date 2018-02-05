ea67e000a3b747119980638fb7fb78da1b985191
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2018-02-04 22:55:47 -0500

    Added charge monitoring

--- src/cp1cli.py
+++ src/cp1cli.py
@@ -1,5 +1,6 @@
 import rospy
 from gazebo_msgs.srv import *
+from std_msgs.msg import Float64
 
 # importing battery services
 from brass_gazebo_battery.srv import *
@@ -19,6 +20,9 @@ class ControlInterface:
         self.set_charging_srv = rospy.ServiceProxy('/mobile_base/set_charging', SetCharging)
         self.set_charge_srv = rospy.ServiceProxy('/mobile_base/set_charge', SetCharge)
         self.set_powerload_srv = rospy.ServiceProxy('/mobile_base/set_power_load', SetLoad)
+
+
+        self.battery_charge = -1
 
 
     def get_bot_state(self):
@@ -44,10 +48,28 @@ class ControlInterface:
         return  self.set_powerload_srv(load)
 
 
+def get_charge(msg):
+    global battery_charge
+    battery_charge = msg.data
+    print(battery_charge)
+
+
+def monitor_battery():
+    rospy.init_node("battery_monitor_client")
+    rospy.Subscriber("/mobile_base/commands/charge_level", Float64, get_charge)
+    rospy.spin()
+
+
 def main():
+
+    global battery_charge
+    battery_charge = -1
+
     ci = ControlInterface()
     state = ci.get_bot_state()
-    print(state)
+    print("Bot is located at ({0}, {1}), facing {2} and going with a speed of {3} m/s".format(state[0], state[1], state[2], state[3]))
+
+    monitor_battery()
 
 
 if __name__ == '__main__':
