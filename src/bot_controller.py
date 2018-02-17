d391f575505020849b073460055e49ec557fc453
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2018-02-17 16:59:00 -0500

    Started writing controller class

---
+++ src/bot_controller.py
@@ -0,0 +1,16 @@
+"""bot controller"""
+
+
+class BotController:
+
+    def __init__(self):
+        pass
+
+    def go_directly(self, start, target= None):
+        pass
+
+    def go_instructions(self, start, target):
+        pass
+
+    def exec_instructions(self, start, end):
+        pass
