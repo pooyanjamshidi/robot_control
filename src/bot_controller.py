4632d1c59b2a21cb818ea0710276e001526b3842
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2018-02-17 19:02:05 -0500

    Added init for bot controller

--- src/bot_controller.py
+++ src/bot_controller.py
@@ -1,10 +1,19 @@
 """bot controller"""
+import os
+from mapserver import MapServer
+from instructions_db import InstructionDB
+from bot_interface import ControlInterface
+
+map_file = os.path.expanduser("~/catkin_ws/src/cp1_base/maps/cp1_map.json")
+instructions_path = os.path.expanduser("~/catkin_ws/src/cp1_base/instructions/")
 
 
 class BotController:
 
     def __init__(self):
-        pass
+        self.map_server = MapServer(map_file)
+        self.instruction_server = InstructionDB(instructions_path)
+        self.gazebo = ControlInterface()
 
     def go_directly(self, start, target= None):
         pass
