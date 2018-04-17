1ea1b68c8004ab57cee06e5d9a5e338452614add
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2018-04-17 14:25:46 -0400

    Adding failure message to the task_finished script

--- scripts/current_task_finished.sh
+++ scripts/current_task_finished.sh
@@ -2,10 +2,16 @@
 
 FILE=~/cp1/current-task-finished
 DONEMSG="DONE"
+FAILEDMSG="FAILED"
 
 if [ -e $FILE ]
 then
-    echo $DONEMSG > $FILE
+    if [ $1 != 0 ]
+    then
+        echo $DONEMSG > $FILE
+    else
+        echo $FAILEDMSG > $FILE
+    fi
 else
     echo "file does not exists"
 fi
